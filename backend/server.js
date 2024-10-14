83c3479487c4154385af5287182e82f8dd1e3e21
2amkoding <2amkoding@gmail.com>
2024-10-14 16:26:01 -0500

    DELETE product functions sucessful

--- backend/server.js
+++ backend/server.js
@@ -30,6 +30,18 @@ app.post("/api/products", async (req, res) => {
     }
 })
 
+app.delete("/api/products/:id", async (req, res) => {
+    const { id } = req.params;
+
+    try{
+        await Product.findByIdAndDelete(id);
+        res.status(200).json({ success: true, message: "Product deleted successfully" });
+    } catch (error) {
+        console.error("Error in Delete product:", error.message);
+        res.status(404).json({ success: false, message: "Product not found" });
+    }
+})
+
 // console.log(process.env.MONGO_URI);
 
 app.listen(5001, () => {
