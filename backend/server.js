ab0d175c133e3ba8e92c4830d0da344a3abd67c0
2amkoding <2amkoding@gmail.com>
2024-10-14 16:50:14 -0500

    getALL products success

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ import express from 'express';
 import dotenv from "dotenv";
 import { connectDB } from './config/db.js';
 import Product from "./models/product.model.js";
+import mongoose from "mongoose";
 
 dotenv.config();
 
@@ -39,6 +40,26 @@ app.post("/api/products", async (req, res) => {
         res.status(500).json({ success: false, message: "Internal server error" });
     }
 })
+
+app.put("/api/products/:id", async (req, res) => {
+
+    const { id } = req.params;
+    const product = req.body;
+
+
+    try{
+
+        if (!mongoose.Types.ObjectId.isValid(id)) {
+            return res.status(404).json({success: false, message: "ProductID not found"});
+        }
+
+        const updatedProduct = await Product.findByIdAndUpdate(id, product, { new: true });
+        res.status(200).json({ success: true, data: updatedProduct });
+    } catch (error) {
+        console.error("Error in Update product:", error.message);
+        res.status(500).json({ success: false, message: "Server error" });
+    }
+});
 
 app.delete("/api/products/:id", async (req, res) => {
     const { id } = req.params;
