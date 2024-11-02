7b19c3835cb81fedf4418bcb1622931c89ffe3c8
2amkoding <2amkoding@gmail.com>
2024-11-02 15:02:59 -0500

    created NavBar, Homepage, CreatePage placeholders

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,11 +3,14 @@ import { createRoot } from 'react-dom/client'
 import React from 'react'
 import App from './App.jsx'
 import { ChakraProvider, defaultSystem } from "@chakra-ui/react"
+import {BrowserRouter} from "react-router-dom";
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
       <ChakraProvider value={defaultSystem}>
+          <BrowserRouter>
           <App />
+          </BrowserRouter>
       </ChakraProvider>
   </StrictMode>
 )
