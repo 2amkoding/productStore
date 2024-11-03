8c076a4e67f6617f3d14e8f198a1915287ff168f
2amkoding <2amkoding@gmail.com>
2024-11-02 20:10:13 -0500

    updated

--- backend/server.js
+++ backend/server.js
@@ -7,7 +7,7 @@ import productRoutes from './routes/product.route.js';
 dotenv.config();
 
 const app = express();
-const PORT = process.env.PORT || 5001;
+const PORT = process.env.PORT
 
 app.use(express.json()); //allows us to accept JSON data in  req.body
 
